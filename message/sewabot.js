0c899aa1383aa2e8774bbc6dd832cd037ed88049
Khairil <101785282+KhairilOFC@users.noreply.github.com>
2022-03-30 11:24:39 +0800

    Update sewabot.js

--- message/sewabot.js
+++ message/sewabot.js
@@ -1,26 +1,33 @@
 
 exports.dada = (prefix, settings, pushname, ucapanWaktu) => {
 return `${ucapanWaktu} kak ${pushname}
-Berikut adalah list harga untuk sewa botz
+Berikut adalah list harga untuk sewa bot
 
-*╭─▸*「 𝗟𝗜𝗦𝗧 𝗦𝗘𝗪𝗔 」
-*┴*
-*│▸* 1 hari = free
-*│▸* 2 hari = Rp 2000         
-*│▸* 7 hari = Rp 5000          
-*│▸* 14 hari = Rp 10000         
-*│▸* 30 hari = Rp 20000                                           
-*┬*
-*╰────────────[*
+╭──「 *_SEWA BOT_* 」
+│❏  1 MINGGU : RM 5
+│❏  1 BULAN : RM 15
+│❏   PERMANEN : -
+╰────
+╭─[ *_KELEBIHAN BOT_*]
+│❏  ＯＮ 24 ＪＡＭ
+│❏  BISA BUAT STICK DLL
+│❏  FITUR BANYAK
+╰────
+╭─[ *_KEUNTUNGAN_* ]
+│❏  ANTI LINK ( AUTO KICK )
+│❏  ANTI VIRTEX ( AUTO KICK )
+│❏  WELCOME ( OTOMATIS )
+│❏  DAN RATUSAN FITUR LAINYA
+╰────
+╭─[ *MINAT CHAT* ]
+│❏  🪀Chat wa.me/601123550412
+╰────
+╭─[ *PAYMENT* ]
+│❏  DIGI
+│❏  MAXIS
+╰────
 
-*╭─▸*「 𝗣𝗔𝗬𝗠𝗘𝗡𝗧 」
-*┴*
-*│▸* Shopee : ${settings.setPayment.Shopee}
-*│▸* Gopay : ${settings.setPayment.Gopay}
-*│▸* Dana : ${settings.setPayment.Dana}
-*│▸* Ovo : ${settings.setPayment.Ovo}
-*┬*
-*╰────────────[*
+*_DI JAMIN ANTI DELAY & ON 24 JAM_*
 
 𝗡𝗢𝗧𝗘 : 
 
